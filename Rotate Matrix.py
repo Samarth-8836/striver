70722ced4c325739dcb81368e012d222dd47609d
Samarth Patel <psamarth8836@gmail.com>
2022-12-16 15:19:42 -0500

    New update on Rotate Matrix

--- Rotate Matrix.py
+++ Rotate Matrix.py
@@ -6,13 +6,13 @@ def rotateOneLevel(matrix, level):
     """
     # Building initial storage
     storage = []
-    for i in range(level, len(matrix) - level, 1):
+    for i in range(level+1, len(matrix) - level, 1):
         storage.append(matrix[level][i])
 
     # Swap storage
     for rotation in range(0, 4, 1):
         print(storage)
-        for i in range(level, len(storage) - level, 1):
+        for i in range(level + 1, len(storage) - level + 1, 1):
             if rotation == 0:
                 # change right most one
                 temp = matrix[i][len(matrix) - 1 - level]
@@ -20,14 +20,14 @@ def rotateOneLevel(matrix, level):
                 storage[i] = temp
             elif rotation == 1:
                 # change bottom most one
-                temp = matrix[len(matrix) - 1 - level][i]
-                matrix[len(matrix) - 1 - level][i] = storage[i]
+                temp = matrix[len(matrix) - 1 - level][len(storage) - level - i]
+                matrix[len(matrix) - 1 - level][len(storage) - level - i] = storage[i]
                 storage[i] = temp
             elif rotation == 2:
                 # change left most one
-                temp = matrix[i][level]
-                matrix[i][level] = storage[i]
-                storage[i] = temp
+                temp = matrix[i - 1][level]
+                matrix[i - 1][level] = storage[i - 1]
+                storage[i - 1] = temp
             elif rotation == 3:
                 # change top most one
                 temp = matrix[level][i]
